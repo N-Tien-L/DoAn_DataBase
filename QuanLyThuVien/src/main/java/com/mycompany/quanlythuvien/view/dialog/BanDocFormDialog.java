5e6f3717e8dfb98b4609c49d813a21d2e8c48002
Le Dac Quoc Anh <ekko.okke666@gmail.com>
2025-11-17 21:22:21 +0700

    Add reader detail panel and update UI for BanDoc

--- QuanLyThuVien/src/main/java/com/mycompany/quanlythuvien/view/dialog/BanDocFormDialog.java
+++ QuanLyThuVien/src/main/java/com/mycompany/quanlythuvien/view/dialog/BanDocFormDialog.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JDialog.java to edit this template
  */
-package com.mycompany.quanlythuvien.view.panel;
+package com.mycompany.quanlythuvien.view.dialog;
 
 import com.mycompany.quanlythuvien.model.BanDoc;
 import javax.swing.JOptionPane;
